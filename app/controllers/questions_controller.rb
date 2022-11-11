df59d43efa3af371d3ec2a085d58c1b5991e567b
Samuel Espinet <samuelespinet30@gmail.com>
2022-11-11 14:03:13 +1000

    done my best

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,5 +3,17 @@ class QuestionsController < ApplicationController
   end
 
   def answer
+    @question = params[:question]
+    @answer = coach(@question)
+  end
+
+  def coach(str)
+    if str == "I am going to work."
+      return "Great"
+    elsif str.end_with?("?")
+      return  "silly question, get dressed for work"
+    else
+      return "I dont't care, get dressed for work"
+    end
   end
 end
